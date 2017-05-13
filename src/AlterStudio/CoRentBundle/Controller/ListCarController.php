d2385356ec6a1898692641279488a58616258f74
Rudi Thoma <admin@ze-tour.com>
2017-05-13 16:21:12 +0200

    message

--- src/AlterStudio/CoRentBundle/Controller/ListCarController.php
+++ src/AlterStudio/CoRentBundle/Controller/ListCarController.php
@@ -4,16 +4,41 @@ namespace AlterStudio\CoRentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 class ListCarController extends Controller
 {
     /**
    * @Security("has_role('ROLE_USER')")
+   *
    */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        //get params
+      $location = $request->query->get('location');
+        $dateD = $request->query->get('date-debut'); // get a $_GET parameter
+      $dateF = $request->query->get('date-fin'); // get a $_GET parameter
+
+      //Declare respitory
+      $em = $this->getDoctrine()->getManager();
+        $annonceRepository = $em->getRepository('AppBundle:Annonce');
+        $qb = $annonceRepository->createQueryBuilder('ann');
+        $query = $qb->join('ann.adresseVoiture', 'adr')
+              ->join('adr.ville', 'vi', 'WITH', $qb->expr()->eq('vi.ville', '?1'))
+              ->addSelect('adr')
+              ->join('ann.calendrier', 'ca')
+              ->where($qb->expr()->between('ca.dateStatus', '?2', '?3'))
+              ->addSelect('ca')
+              ->setParameter(1, $location)
+              ->setParameter(2, $dateD)
+              ->setParameter(3, $dateD)
+              ->getQuery();
+
+        
+        $annonces = $query->getResult();
+
         return $this->render('corent/list.html.twig', array(
-            // ...
+            "annonces"=>$annonces
         ));
     }
 }
